fc2c913a5e60adc6a4b5e6b1790426656023a1ea
Chris Gkoumas <cgkoum01@ucy.ac.cy>
2021-08-05 17:32:36 +0300

    Update memory scripts 5/8

---
+++ Memory Scripts/Mem_Event_Detection.py
@@ -0,0 +1,110 @@
+''' Event Detection and collation into separate files'''
+
+import os
+import itertools
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from helper import *
+
+
+# Categorize fixations and saccades based on their order:
+for i,k in itertools.product(sub_id, img_id):
+    file='Sub_'+str(i)+'_Image_'+str(k)+'.csv'
+    dataset=pd.read_csv(os.path.join(TRIALS_PATH,file),low_memory=False)
+    s = dataset['Fix_or_Sac']
+    m = s != s.shift()
+    r = m.cumsum().groupby(s).rank(method='dense')
+    dataset['Group'] =  s +"_"+ r.astype(int).astype(str)
+    dataset.to_csv(os.path.join(TRIALS_PATH,file), index=False)
+
+
+#Create separate files for each participant's fixations:
+for i,k in itertools.product(sub_id, img_id):
+    file='Sub_'+str(i)+'_Image_'+str(k)+'.csv'
+    dataset=pd.read_csv(os.path.join(TRIALS_PATH,file),low_memory=False)
+    events=pd.DataFrame()
+    events['Fixation_Start']=dataset.groupby('Group')['TIME'].min()
+    events['Event_ID']= dataset.groupby('Group')['Group'].first()
+    only_fixations=dataset.query("Group.str.startswith('F').values")
+    time_fix_max=only_fixations.groupby('Group')['TIME'].max()
+    time_fix_min=only_fixations.groupby('Group')['TIME'].min()
+    events['FPOG_DUR']=time_fix_max-time_fix_min
+    events['FPOG_X']=only_fixations.groupby('Group')['BPOGX'].mean()
+    events['FPOG_Y']=only_fixations.groupby('Group')['BPOGY'].mean()
+    #events['event'] = events['Event_ID'].str.rsplit("_").str[0]
+    events['Idx'] = events['Event_ID'].str.rsplit("_").str[-1].astype(int)
+    events.sort_values('Idx',inplace=True)
+    events.drop('Idx', axis = 1,inplace=True)
+    #final=events[(events['FPOG_DUR']> 0.050) | (events['SAC_DUR'] > 0.010)]
+    to_write=events[events['FPOG_DUR']> 0.050]
+    to_write.loc[to_write.index[0], 'Trial_Start'] = dataset['TIME'].iloc[0]
+    to_write.to_csv(os.path.join(EVENTS_PATH,file), index=False)
+    
+
+#Add columns about saccades to the files created above:
+for i,k in itertools.product(sub_id, img_id):
+    file='Sub_'+str(i)+'_Image_'+str(k)+'.csv'
+    dataset=pd.read_csv(os.path.join(EVENTS_PATH,file),low_memory=False)
+    trial_start=dataset.iloc[0,5]
+    first_fixation_start=dataset.iloc[0,0]
+    first_fixation_dur=dataset.iloc[0,2]
+    dataset.loc[dataset.index[0],'SAC_LATENCY']=first_fixation_dur \
+        if trial_start==first_fixation_start \
+            else (first_fixation_start-trial_start) + first_fixation_dur
+    x=dataset['FPOG_X'].diff()
+    y=dataset['FPOG_Y'].diff()
+    dataset['SAC_AMPLITUDE']=(x ** 2 + y ** 2) ** 0.5
+    fix_dur_wo_last=dataset.iloc[:-1,2].reset_index(drop=True)
+    fix_start_dif=dataset['Fixation_Start'].diff().dropna().reset_index(drop=True)
+    dataset['SAC_DUR']=fix_start_dif-fix_dur_wo_last
+    dataset.to_csv(os.path.join(EVENTS_PATH,file), index=False)
+    
+    
+    '''sac_dur_min=dataset['SAC_DUR'].min() *1000
+    if sac_dur_min<10:
+     print(f'subject: {str(i)}, Image: {str(k)}')'''
+
+
+#Visualize scanpath for all participants based on I-VT fixations:
+for h,j in itertools.product(sub_id,img_id):
+    file='Sub_'+str(h)+'_Image_'+str(j)+'.csv'
+    dataset=pd.read_csv(os.path.join(EVENTS_PATH,file),low_memory=False)
+    x=dataset['FPOG_X']
+    y=dataset['FPOG_Y']
+    fix_dur=dataset['FPOG_DUR']
+    fig, ax = plt.subplots(figsize=(20, 11))
+    ax.scatter(x,
+               y,
+               zorder=1
+               ,marker='o',
+               s=fix_dur*10000,
+               color='lime',
+               alpha=0.5)
+    ax.plot(x,
+            y,
+            '-o',
+            linewidth=3,
+            color='blue')
+    img = plt.imread(IMG_PATH+"\S"+str(j)+".jpg")
+    plt.imshow(img, 
+               zorder=0, 
+               extent=[-960, 960, -540, 540],
+               aspect='auto')
+    for i in range(len(fix_dur)):
+        ax.annotate(str(i+1),
+                    xy=(fix_dur.iloc[i],
+                        fix_dur.iloc[i]),
+                    xytext=(x.iloc[i], 
+                            y.iloc[i]),
+                    fontsize=30,
+                    color='black',
+                    ha='center',
+                    va='center')
+    plt.xlabel('X coordinates (in pixels)', size=20)
+    plt.ylabel('Y coordinates (in pixels)', size=20)
+    plt.title('Scanpath for Subject '+str(h)+' , Image '+str(j), size=30)
+    my_img='Subject_'+str(h)+'_Image_'+str(j)+'.png'
+    fig.savefig(os.path.join(IVT_SCANPATH,my_img))
+    #plt.show()
+    plt.close()
---
+++ Memory Scripts/helper.py
@@ -0,0 +1,32 @@
+screen_resX=1920
+
+screen_resY=1080
+
+img_id=[1,2,7,12,13,15,19,25,27,29,41,42,43,44,48,49,51,54,55,59,61,
+            64,67,74,76,77,84,87,88,91,94,95,100,101,112,113,3,6,10,17,
+            21,23,28,33,35,38,39,40,46,50,52,58,60,62,63,70,72,73,75,78,
+            80,82,89,90,92,97,99,102,103,104,105,108]
+
+sub_id=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,25,26,
+        28,29,30,31]
+
+TRIALS_PATH='C:/Users/presi/Desktop/PhD/Memory guided attention in '\
+        'cluttered scenes v.3/Eye Tracking Data/2. Memory/Mem_trials'
+
+IMG_PATH='C:/Users/presi/Desktop/PhD/Memory guided attention in '\
+        'cluttered scenes v.3/Tasks/Task2/Scenes'
+
+EVENTS_PATH='C:/Users/presi/Desktop/PhD/Memory guided attention in '\
+        'cluttered scenes v.3/Eye Tracking Data/2. Memory/Mem_events'
+
+DATA_PATH='C:/Users/presi/Desktop/PhD/Memory guided attention in '\
+    'cluttered scenes v.3/Eye Tracking Data/2. Memory/Raw'
+    
+RAW_SCANPATH='C:/Users/presi/Desktop/PhD/Memory guided attention in '\
+    'cluttered scenes v.3/Eye Tracking Data/2. Memory/Mem_ visualizations/'\
+        'Raw_Scanpaths'
+        
+IVT_SCANPATH='C:/Users/presi/Desktop/PhD/Memory guided attention in '\
+    'cluttered scenes v.3/Eye Tracking Data/2. Memory/Mem_ visualizations/'\
+        'IVT_Scanpaths'
+        
